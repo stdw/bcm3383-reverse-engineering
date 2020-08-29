ae3d49771a3987a6274681e1ad2728b454fd46bd
user <user@localhost.localdomain>
2020-08-28 20:05:59 -0400

    use proper abi

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-mips-linux-gcc main.c -o exe -mips32 -T ./ldscript -fno-builtin -nostdlib -nodefaultlibs -nostartfiles
+mips-linux-gcc main.c -o exe -march=mips32 -mabi=eabi -mno-abicalls -T ./ldscript -fno-builtin -nostdlib -nodefaultlibs -nostartfiles
 mips-linux-objcopy -O binary -j .text -j .data -j .rodata exe bin
  
